b33c5caef1af12626441258337998d37ae6d6aac
gart09 <gart09@naver.com>
2025-05-30 01:03:26 +0900

    feat: 앱데이터소스에 엔티티추가

--- src/config/mysql.config.ts
+++ src/config/mysql.config.ts
@@ -4,6 +4,8 @@ import {Tag} from "../entity/tag.entity";
 import {Term} from "../entity/term.entity";
 import {TermVersion} from "../entity/termVersion.entity";
 import {Banner} from "../entity/banner.entity";
+import {ReportEntity} from "../entity/report.entity";
+import {SanctionGame, SanctionUser} from "../entity/sanction.entity";
 
 const HOST = process.env.DB_HOST || 'localhost';
 const PORT = Number(process.env.DB_PORT) || 3306;
@@ -20,7 +22,7 @@ const AppDataSource = new DataSource({
     database: DB_NAME,
     synchronize: false,
     logging: true,
-    entities: [ Tag, Term, TermVersion, Banner ],
+    entities: [ Tag, Term, TermVersion, Banner, Account, ReportEntity, SanctionUser, SanctionGame],
     migrations: [],
     subscribers: [],
 });
