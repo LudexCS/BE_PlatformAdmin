1f106efa8e9fbd5a3a7f1375ddbdd08223477b2d
gart09 <gart09@naver.com>
2025-06-04 19:55:50 +0900

    fix: ㅅㅇㄱㅈㅅ

--- src/route/searchUser.route.ts
+++ src/route/searchUser.route.ts
@@ -34,7 +34,7 @@ const router: Router = Router();
 
 /**
  * @swagger
- * /platformadmin/api/admin/user/usersList:
+ * /api/admin/user/usersList:
  *   get:
  *     summary: 전체 유저 목록 조회
  *     description: 페이지네이션을 기반으로 전체 유저 리스트를 조회합니다.
@@ -86,7 +86,7 @@ router.get("/usersList", async (req: Request, res: Response) => {
 
 /**
  * @swagger
- * /platformadmin/api/admin/user/userDetail:
+ * /api/admin/user/userDetail:
  *   get:
  *     summary: 유저 정보 상세 조회
  *     description: userId를 기준으로 유저의 상세 정보를 조회합니다.
@@ -128,6 +128,6 @@ router.get("/userDetail", async (req: Request, res: Response) => {
             res.status(400).json({message: "Unknown error"});
         }
     }
-})
+});
 
 export default router;
